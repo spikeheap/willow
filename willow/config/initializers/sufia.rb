150796fafd736597b3f880ad512249619b4e2744
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2017-03-30 02:50:51 +0100

    Generated new dataset work

--- willow/config/initializers/sufia.rb
+++ willow/config/initializers/sufia.rb
@@ -1,6 +1,8 @@
 Sufia.config do |config|
   # Injected via `rails g sufia:work Work`
   config.register_curation_concern :work
+  # Injected via `rails g sufia:work Dataset`
+  config.register_curation_concern :dataset
   # Email recipient of messages sent via the contact form
   # config.contact_email = "repo-admin@example.org"
 
